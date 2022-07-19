0e8826fc9c801a8bff6842ee57962cdb76dcae5b
CWard19 <107571255+CWard19@users.noreply.github.com>
2022-07-19 17:43:08 +0100

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import seaborn as sns
+import pandas as pd
+
+df = pd.DataFrame({"country":["IE", "UK"], "population": [5.5, 50]})
+
+sns.barplot(x="population", y="country", data=df)
+
